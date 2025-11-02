ab0d353e8d4217cf0fb3a4af01e3251f6890b0f8
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-11-02 23:39:22 +0800

    Create entities for 'base/accounts/customer'.

---
+++ Chapter-02-building-microservice/accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+//Like Parents of all entities.
+@MappedSuperclass
+//@EntityListeners(AuditingEntityListener.class)
+//From Lombok
+@Getter
+@Setter
+@ToString
+public class BaseEntity {
+
+    //    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    //    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    //    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    //    @LastModifiedBy
+    @Column(insertable = false)
+    private String updatedBy;
+}
