24811755f68ecd24c467098605f8fab58883b668
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-11-06 22:50:48 +0800

    Set the service-impl to create customer&account.

--- Chapter-02-building-microservice/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ Chapter-02-building-microservice/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,19 @@
 package com.eazybytes.accounts.service.impl;
 
+import com.eazybytes.accounts.constants.AccountsConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +24,35 @@ public class AccountsServiceImpl implements IAccountsService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+//        if (optionalCustomer.isPresent()) {
+//            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+//                    + customerDto.getMobileNumber());
+//        }
 
+        //Save Customer
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Farmer");
+        Customer savedCustomer = customerRepository.save(customer);
+        //Save Account
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    /**
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedAt(LocalDateTime.now());
+        newAccount.setCreatedBy("Farmer");
+        return newAccount;
     }
 }
